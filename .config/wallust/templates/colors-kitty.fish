1375303ef0b4cef4c63bb890487db8d904a3a646
Liuran Shen <3114575233@qq.com>
2026-04-06 18:51:06 +0800

    feat(wallust-kitty): 添加kitty的wallust配置
    + 添加kitty模板
    + 为kitty.conf添加主题文件

---
+++ .config/wallust/templates/colors-kitty.fish
@@ -0,0 +1,40 @@
+# 由wallust自动生成，请勿手动修改
+# 背景/前景色，和壁纸完全匹配
+background {{background}}
+foreground {{foreground}}
+selection_background {{color5}}
+selection_foreground {{background}}
+cursor {{color15}}
+cursor_text_color {{foreground}}
+
+# 标准16色终端配色，fastfetch自动继承这些颜色
+color0 {{color0}}
+color8 {{color8}}
+
+#: black
+color1 {{color1}}
+color9 {{color9}}
+
+#: red
+color2 {{color2}}
+color10 {{color10}}
+
+#: green
+color3 {{color3}}
+color11 {{color11}}
+
+#: yellow
+color4 {{color4}}
+color12 {{color12}}
+
+#: blue
+color5 {{color5}}
+color13 {{color13}}
+
+#: magenta
+color6 {{color6}}
+color14 {{color14}}
+
+#: cyan
+color7 {{color7}}
+color15 {{color15}}
